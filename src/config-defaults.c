dcd46cb21ef63f7fa8fc9ead22981c8b5a37502a
Grigory Kirillov <txgk@bk.ru>
2021-08-03 19:39:32 +0300

    do not show list menu entry number by default

--- src/config-defaults.c
+++ src/config-defaults.c
@@ -3,7 +3,7 @@
 size_t config_max_items = 100; // 0 == inf
 size_t config_init_parser_buf_size = 100000;
 
-bool config_menu_show_number = true;
+bool config_menu_show_number = false;
 bool config_menu_show_decoration_number = false;
 
 char *config_contents_meta_data = "feed,title,author,category,date,url,comments";

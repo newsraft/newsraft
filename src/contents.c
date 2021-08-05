7a43caa30a99b0951653c0d3c7eefd33b291e5c6
Grigory Kirillov <txgk@bk.ru>
2021-08-06 01:31:39 +0300

    cleanup

--- src/contents.c
+++ src/contents.c
@@ -77,7 +77,7 @@ calculate_pad_height_for_wstring(struct wstring *wstr)
 }
 
 static int
-calculate_pad_height_for_buf(struct string *buf)
+calculate_pad_height_for_string(struct string *buf)
 {
 	int pad_height = 1, not_newline = 0;
 	for (size_t i = 0; i < buf->len; ++i) {
@@ -91,17 +91,79 @@ calculate_pad_height_for_buf(struct string *buf)
 	return pad_height;
 }
 
-/* in some cases calculate_pad_height_for_wcs returns wrong number of lines
-* needed for pad to store all data of buf.
-* TODO fix these two functions above, please...
-* for now use this function to resize pad everytime there is so much data in buffer */
+/* in some cases calculate_pad_height_for_wstring and calculate_pad_height_for_string
+ * functions are returning wrong number of lines needed for pad to store all data of buf.
+ * TODO >> fix these two functions above, please << TODO
+ * but for now use this function to resize pad everytime there is so much data in buffer. */
 void
 make_sure_pad_has_enough_lines(WINDOW *pad, int needed_lines)
 {
 	if (needed_lines > pad_height) {
+		debug_write(DBG_WARN, "pad had to expand from %d to %d lines!\n", pad_height, needed_lines);
 		pad_height = needed_lines;
 		wresize(pad, pad_height, COLS);
-		debug_write(DBG_WARN, "pad had to expand!\n");
+	}
+}
+
+static void
+write_wstring_to_pad_without_linebreaks(WINDOW *pad, struct wstring *wbuf)
+{
+	int newlines = 0;
+	wchar_t *iter = wbuf->ptr;
+	wchar_t *newline_char;
+	while (1) {
+		newline_char = wcschr(iter, L'\n');
+		if (newline_char != NULL) {
+			while (newline_char - iter > COLS) {
+				make_sure_pad_has_enough_lines(pad, newlines + 1);
+				wmove(pad, newlines++, 0);
+				waddnwstr(pad, iter, COLS);
+				iter += COLS;
+			}
+			make_sure_pad_has_enough_lines(pad, newlines + 1);
+			wmove(pad, newlines++, 0);
+			waddnwstr(pad, iter, newline_char - iter);
+			iter = newline_char + 1;
+		} else {
+			while (iter < wbuf->ptr + wbuf->len) {
+				make_sure_pad_has_enough_lines(pad, newlines + 1);
+				wmove(pad, newlines++, 0);
+				waddnwstr(pad, iter, COLS);
+				iter += COLS;
+			}
+			break;
+		}
+	}
+}
+
+static void
+write_string_to_pad_without_linebreaks(WINDOW *pad, struct string *buf)
+{
+	int newlines = 0;
+	char *iter = buf->ptr;
+	char *newline_char;
+	while (1) {
+		newline_char = strchr(iter, '\n');
+		if (newline_char != NULL) {
+			while (newline_char - iter > COLS) {
+				make_sure_pad_has_enough_lines(pad, newlines + 1);
+				wmove(pad, newlines++, 0);
+				waddnstr(pad, iter, COLS);
+				iter += COLS;
+			}
+			make_sure_pad_has_enough_lines(pad, newlines + 1);
+			wmove(pad, newlines++, 0);
+			waddnstr(pad, iter, newline_char - iter);
+			iter = newline_char + 1;
+		} else {
+			while (iter < buf->ptr + buf->len) {
+				make_sure_pad_has_enough_lines(pad, newlines + 1);
+				wmove(pad, newlines++, 0);
+				waddnstr(pad, iter, COLS);
+				iter += COLS;
+			}
+			break;
+		}
 	}
 }
 
@@ -109,7 +171,6 @@ static WINDOW *
 create_contents_window(struct string *buf)
 {
 	WINDOW *pad;
-	int newlines = 0;
 	if (1) { // buffer is multi-byte
 		struct wstring *wbuf = convert_string_to_wstring(buf);
 		if (wbuf == NULL) {
@@ -124,65 +185,19 @@ create_contents_window(struct string *buf)
 			return NULL;
 		}
 
-		wchar_t *iter = wbuf->ptr;
-		wchar_t *newline_char;
-		while (1) {
-			newline_char = wcschr(iter, L'\n');
-			if (newline_char != NULL) {
-				while (newline_char - iter > COLS) {
-					make_sure_pad_has_enough_lines(pad, newlines + 1);
-					wmove(pad, newlines++, 0);
-					waddnwstr(pad, iter, COLS);
-					iter += COLS;
-				}
-				make_sure_pad_has_enough_lines(pad, newlines + 1);
-				wmove(pad, newlines++, 0);
-				waddnwstr(pad, iter, newline_char - iter);
-				iter = newline_char + 1;
-			} else {
-				while (iter < wbuf->ptr + wbuf->len) {
-					make_sure_pad_has_enough_lines(pad, newlines + 1);
-					wmove(pad, newlines++, 0);
-					waddnwstr(pad, iter, COLS);
-					iter += COLS;
-				}
-				break;
-			}
-		}
+		write_wstring_to_pad_without_linebreaks(pad, wbuf);
+
 		free_wstring(wbuf);
 	} else { // buffer is single-byte
-		pad_height = calculate_pad_height_for_buf(buf);
+		pad_height = calculate_pad_height_for_string(buf);
 		pad = newpad(pad_height, COLS);
 		if (pad == NULL) {
 			debug_write(DBG_ERR, "could not create pad window for item contents\n");
 			return NULL;
 		}
-		debug_write(DBG_INFO, "created pad of %d lines height\n", pad_height);
 
-		char *iter = buf->ptr;
-		char *newline_char;
-		while (1) {
-			newline_char = strchr(iter, '\n');
-			if (newline_char != NULL) {
-				while (newline_char - iter > COLS) {
-					wmove(pad, newlines++, 0);
-					waddnstr(pad, iter, COLS);
-					iter += COLS;
-				}
-				wmove(pad, newlines++, 0);
-				waddnstr(pad, iter, newline_char - iter);
-				iter = newline_char + 1;
-			} else {
-				while (iter < buf->ptr + buf->len) {
-					wmove(pad, newlines++, 0);
-					waddnstr(pad, iter, COLS);
-					iter += COLS;
-				}
-				break;
-			}
-		}
+		write_string_to_pad_without_linebreaks(pad, buf);
 	}
-
 	return pad;
 }
 

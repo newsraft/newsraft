ed42275375b26ade58f31b8dbefd904f99fdf581
Grigory Kirillov <txgk@bk.ru>
2021-10-01 23:30:51 +0300

    do not use environment for configuration

--- src/path.c
+++ src/path.c
@@ -7,18 +7,15 @@
 #include <sys/stat.h>
 #include "feedeater.h"
 
+// Note to the future.
+// Do not read feedeater-specific file pathes from environment variables (like FEEDEATER_CONFIG_DIR),
+// because environment is intended for settings that are valueable to many programs, not just a single one.
+
 #define MAXPATH 4096 // maximum length of path
 
 #define FEEDS_FILE_NAME "feeds"
 #define CONFIG_FILE_NAME "config"
 #define DB_FILE_NAME "feedeater.sqlite3"
-
-#define FEEDS_FILE_PATH_ENVVAR "FEEDEATER_FEEDS_PATH"
-#define CONFIG_FILE_PATH_ENVVAR "FEEDEATER_CONFIG_PATH"
-#define DB_FILE_PATH_ENVVAR "FEEDEATER_DB_PATH"
-
-#define CONF_DIR_PATH_ENVVAR "FEEDEATER_CONF_DIR"
-#define DATA_DIR_PATH_ENVVAR "FEEDEATER_DATA_DIR"
 
 static char *feeds_file_path = NULL;
 static char *config_file_path = NULL;
@@ -34,24 +31,9 @@ get_conf_dir_path(void)
 	}
 
 	DIR *d;
-	char *env_var = getenv(CONF_DIR_PATH_ENVVAR);
 	size_t env_var_len;
-	if (env_var != NULL) {
-		env_var_len = strlen(env_var);
-		if (env_var_len != 0) {
-			d = opendir(env_var);
-			if (d != NULL) {
-				closedir(d);
-				strcpy(conf_dir_path, env_var);
-				return conf_dir_path;
-			}
-			fprintf(stderr, CONF_DIR_PATH_ENVVAR " holds invalid path\n");
-			free(conf_dir_path);
-			return NULL;
-		}
-	}
 
-	env_var = getenv("XDG_CONFIG_HOME");
+	char *env_var = getenv("XDG_CONFIG_HOME");
 	if (env_var != NULL) {
 		env_var_len = strlen(env_var);
 		if (env_var_len != 0) {
@@ -101,24 +83,9 @@ get_data_dir_path(void)
 	}
 
 	DIR *d;
-	char *env_var = getenv(DATA_DIR_PATH_ENVVAR);
 	size_t env_var_len;
-	if (env_var != NULL) {
-		env_var_len = strlen(env_var);
-		if (env_var_len != 0) {
-			d = opendir(env_var);
-			if (d != NULL) {
-				closedir(d);
-				strcpy(data_dir_path, env_var);
-				return data_dir_path;
-			}
-			fprintf(stderr, DATA_DIR_PATH_ENVVAR " holds invalid path\n");
-			free(data_dir_path);
-			return NULL;
-		}
-	}
 
-	env_var = getenv("XDG_DATA_HOME");
+	char *env_var = getenv("XDG_DATA_HOME");
 	if (env_var != NULL) {
 		env_var_len = strlen(env_var);
 		if (env_var_len != 0) {
@@ -198,20 +165,7 @@ get_feeds_path(void)
 		return feeds_file_path;
 	}
 
-	char *feeds_path = getenv(FEEDS_FILE_PATH_ENVVAR);
-	if (feeds_path != NULL) {
-		size_t feeds_path_len = strlen(feeds_path);
-		if (feeds_path_len != 0) {
-			feeds_file_path = malloc(sizeof(char) * (feeds_path_len + 1));
-			if (feeds_file_path == NULL) {
-				return NULL;
-			}
-			strcpy(feeds_file_path, feeds_path);
-			return feeds_file_path;
-		}
-	}
-
-	feeds_path = get_conf_dir_path();
+	char *feeds_path = get_conf_dir_path();
 	if (feeds_path != NULL) {
 		strcat(feeds_path, "/" FEEDS_FILE_NAME);
 		return feeds_path;
@@ -227,20 +181,7 @@ get_config_path(void)
 		return config_file_path;
 	}
 
-	char *config_path = getenv(CONFIG_FILE_PATH_ENVVAR);
-	if (config_path != NULL) {
-		size_t config_path_len = strlen(config_path);
-		if (config_path_len != 0) {
-			config_file_path = malloc(sizeof(char) * (config_path_len + 1));
-			if (config_file_path == NULL) {
-				return NULL;
-			}
-			strcpy(config_file_path, config_path);
-			return config_file_path;
-		}
-	}
-
-	config_path = get_conf_dir_path();
+	char *config_path = get_conf_dir_path();
 	if (config_path != NULL) {
 		strcat(config_path, "/" CONFIG_FILE_NAME);
 		return config_path;
@@ -256,20 +197,7 @@ get_db_path(void)
 		return db_file_path;
 	}
 
-	char *db_path = getenv(DB_FILE_PATH_ENVVAR);
-	if (db_path != NULL) {
-		size_t db_path_len = strlen(db_path);
-		if (db_path_len != 0) {
-			db_file_path = malloc(sizeof(char) * (db_path_len + 1));
-			if (db_file_path == NULL) {
-				return NULL;
-			}
-			strcpy(db_file_path, db_path);
-			return db_file_path;
-		}
-	}
-
-	db_path = get_data_dir_path();
+	char *db_path = get_data_dir_path();
 	if (db_path != NULL) {
 		strcat(db_path, "/" DB_FILE_NAME);
 		return db_path;

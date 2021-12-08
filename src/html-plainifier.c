9fce391511d8f57bdab8be42e57f236ad5b62c49
Grigory Kirillov <txgk@bk.ru>
2021-12-08 15:32:29 +0300

    improve html plainifier: make tag buffer dynamic, fix nested lists formatting, add MAX_NESTED_LISTS_DEPTH and some cosmetic stuff

--- src/html-plainifier.c
+++ src/html-plainifier.c
@@ -2,17 +2,18 @@
 #include <string.h>
 #include "feedeater.h"
 
+#define INITIAL_TAG_BUFFER_SIZE 1000 // must be greater than 1
+#define MAX_NESTED_LISTS_DEPTH 10
+
+static void add_one_newline(void);
+static void add_two_newlines(void);
+static void add_three_newlines(void);
 static void br_handler(void);
 static void hr_handler(void);
-static void block_start_handler(void);
-static void block_end_handler(void);
-static void heading_start_handler(void);
-static void heading_end_handler(void);
 static void li_start_handler(void);
 static void ul_start_handler(void);
 static void ul_end_handler(void);
 static void ol_start_handler(void);
-static void ol_end_handler(void);
 static void sup_start_handler(void);
 static void q_start_handler(void);
 
@@ -35,69 +36,69 @@ struct tag_handler {
 	void (*end_handler)(void);
 };
 
+struct list_level {
+	enum list_type type;
+	int length;
+};
+
 struct tag_handler tag_handlers[] = {
-	{"a",          HTML_NONE,   NULL,                  NULL}, // TODO
-	{"b",          HTML_NONE,   NULL,                  NULL}, // TODO
-	{"i",          HTML_NONE,   NULL,                  NULL}, // TODO
-	{"em",         HTML_NONE,   NULL,                  NULL}, // TODO
-	{"mark",       HTML_NONE,   NULL,                  NULL}, // TODO
-	{"small",      HTML_NONE,   NULL,                  NULL}, // TODO
-	{"time",       HTML_NONE,   NULL,                  NULL}, // TODO
-	{"strong",     HTML_NONE,   NULL,                  NULL}, // TODO
-	{"p",          HTML_NONE,   heading_end_handler,   heading_end_handler},
-	{"details",    HTML_NONE,   heading_end_handler,   heading_end_handler},
-	{"blockquote", HTML_NONE,   heading_end_handler,   heading_end_handler},
-	{"div",        HTML_NONE,   block_start_handler,   block_end_handler},
-	{"summary",    HTML_NONE,   block_start_handler,   block_end_handler},
-	{"span",       HTML_NONE,   NULL,                  NULL},
-	{"code",       HTML_NONE,   NULL,                  NULL},
-	{"pre",        HTML_PRE,    heading_end_handler,   heading_end_handler},
-	{"li",         HTML_NONE,   li_start_handler,      block_end_handler},
-	{"ul",         HTML_NONE,   ul_start_handler,      ul_end_handler},
-	{"ol",         HTML_NONE,   ol_start_handler,      ol_end_handler},
-	{"br",         HTML_NONE,   br_handler,            NULL},
-	{"hr",         HTML_NONE,   hr_handler,            NULL},
-	{"h1",         HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"h2",         HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"h3",         HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"h4",         HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"h5",         HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"h6",         HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"sup",        HTML_NONE,   sup_start_handler,     NULL},
-	{"q",          HTML_NONE,   q_start_handler,       q_start_handler},
-	{"section",    HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"footer",     HTML_NONE,   heading_start_handler, heading_end_handler},
-	{"style",      HTML_STYLE,  NULL,                  NULL},
-	{"script",     HTML_SCRIPT, NULL,                  NULL},
+	{"span",       HTML_NONE,   NULL,                NULL},
+	{"code",       HTML_NONE,   NULL,                NULL}, // TODO
+	{"a",          HTML_NONE,   NULL,                NULL}, // TODO
+	{"b",          HTML_NONE,   NULL,                NULL}, // TODO
+	{"i",          HTML_NONE,   NULL,                NULL}, // TODO
+	{"em",         HTML_NONE,   NULL,                NULL}, // TODO
+	{"mark",       HTML_NONE,   NULL,                NULL}, // TODO
+	{"small",      HTML_NONE,   NULL,                NULL}, // TODO
+	{"time",       HTML_NONE,   NULL,                NULL}, // TODO
+	{"strong",     HTML_NONE,   NULL,                NULL}, // TODO
+	{"p",          HTML_NONE,   add_two_newlines,    add_two_newlines},
+	{"details",    HTML_NONE,   add_two_newlines,    add_two_newlines},
+	{"blockquote", HTML_NONE,   add_two_newlines,    add_two_newlines},
+	{"pre",        HTML_PRE,    add_two_newlines,    add_two_newlines},
+	{"div",        HTML_NONE,   add_one_newline,     add_one_newline},
+	{"section",    HTML_NONE,   add_one_newline,     add_one_newline},
+	{"footer",     HTML_NONE,   add_one_newline,     add_one_newline},
+	{"summary",    HTML_NONE,   add_one_newline,     add_one_newline},
+	{"li",         HTML_NONE,   li_start_handler,    add_one_newline},
+	{"ul",         HTML_NONE,   ul_start_handler,    ul_end_handler},
+	{"ol",         HTML_NONE,   ol_start_handler,    ul_end_handler}, // it is not a typo
+	{"br",         HTML_NONE,   br_handler,          NULL},
+	{"hr",         HTML_NONE,   hr_handler,          NULL},
+	{"h1",         HTML_NONE,   add_three_newlines,  add_two_newlines},
+	{"h2",         HTML_NONE,   add_three_newlines,  add_two_newlines},
+	{"h3",         HTML_NONE,   add_three_newlines,  add_two_newlines},
+	{"h4",         HTML_NONE,   add_three_newlines,  add_two_newlines},
+	{"h5",         HTML_NONE,   add_three_newlines,  add_two_newlines},
+	{"h6",         HTML_NONE,   add_three_newlines,  add_two_newlines},
+	{"sup",        HTML_NONE,   sup_start_handler,   NULL},
+	{"q",          HTML_NONE,   q_start_handler,     q_start_handler},
+	{"style",      HTML_STYLE,  NULL,                NULL},
+	{"script",     HTML_SCRIPT, NULL,                NULL},
 
 };
 
 // Well...
 // Functions down there will be called hella lot of times per second!
 // Do we really need to ask our poor stack every single time to allocate?
-// This is real question, but for now leave everything in
-// the lifetime of the program because this might be a ?good? ?performance? ?boost?
-static enum list_type current_list_type = UNORDERED_LIST;
+// This is real question, but for now leave everything in the lifetime of
+// the program because this might be a ?good? ?performance? ?boost?
 static char *text;
 static size_t text_len;
 static char *tag_name;
 static size_t tag_name_len;
 static bool is_end_tag;
 static uint8_t list_depth;
-static uint8_t lists_length[100]; // make dynamic TODO
+static struct list_level list_levels[MAX_NESTED_LISTS_DEPTH];
 static enum html_position position;
-static char temp;
-static char tag[1000]; // make dynamic TODO
+static char *tag;
 static size_t tag_len;
+static size_t tag_lim;
+static char temp;
+static char *temp_ptr;
 
 static void
-br_handler(void)
-{
-	text[text_len++] = '\n';
-}
-
-static void
-block_start_handler(void)
+add_one_newline(void)
 {
 	if (text_len >= 1 && text[text_len - 1] == '\n') {
 		// nothing
@@ -107,27 +108,22 @@ block_start_handler(void)
 }
 
 static void
-block_end_handler(void)
+add_two_newlines(void)
 {
 	if (text_len >= 1 && text[text_len - 1] == '\n') {
-		// nothing
+		if (text_len >= 2 && text[text_len - 2] == '\n') {
+			// nothing
+		} else {
+			text[text_len++] = '\n';
+		}
 	} else {
+		text[text_len++] = '\n';
 		text[text_len++] = '\n';
 	}
 }
 
 static void
-hr_handler(void)
-{
-	block_start_handler();
-	for (int i = 0; i < list_menu_width; ++i) {
-		text[text_len++] = '-';
-	}
-	text[text_len++] = '\n';
-}
-
-static void
-heading_start_handler(void)
+add_three_newlines(void)
 {
 	if (text_len >= 1 && text[text_len - 1] == '\n') {
 		if (text_len >= 2 && text[text_len - 2] == '\n') {
@@ -148,38 +144,39 @@ heading_start_handler(void)
 }
 
 static void
-heading_end_handler(void)
+br_handler(void)
 {
-	if (text_len >= 1 && text[text_len - 1] == '\n') {
-		if (text_len >= 2 && text[text_len - 2] == '\n') {
-			// nothing
-		} else {
-			text[text_len++] = '\n';
-		}
-	} else {
-		text[text_len++] = '\n';
-		text[text_len++] = '\n';
+	text[text_len++] = '\n';
+}
+
+static void
+hr_handler(void)
+{
+	add_one_newline();
+	for (int i = 0; i < list_menu_width; ++i) {
+		text[text_len++] = '-';
 	}
+	text[text_len++] = '\n';
 }
 
 static void
 li_start_handler(void)
 {
-	block_start_handler();
+	add_one_newline();
 	for (uint8_t i = 0; i < list_depth; ++i) {
 		text[text_len++] = ' ';
 		text[text_len++] = ' ';
 		text[text_len++] = ' ';
 	}
-	if (current_list_type == UNORDERED_LIST) {
+	if (list_levels[list_depth - 1].type == UNORDERED_LIST) {
 		text[text_len++] = '*';
 		text[text_len++] = ' ';
 		text[text_len++] = ' ';
 	} else {
-		++lists_length[list_depth - 1];
+		++(list_levels[list_depth - 1].length);
 		char *number_str = malloc(sizeof(char) * 10);
 		if (number_str != NULL) {
-			snprintf(number_str, 10, "%d. ", lists_length[list_depth - 1]);
+			snprintf(number_str, 10, "%d. ", list_levels[list_depth - 1].length);
 			text[text_len] = '\0';
 			strcat(text, number_str);
 			text_len += strlen(number_str);
@@ -191,33 +188,33 @@ li_start_handler(void)
 static void
 ul_start_handler(void)
 {
-	heading_end_handler();
-	current_list_type = UNORDERED_LIST;
+	if (list_depth == MAX_NESTED_LISTS_DEPTH) {
+		return;
+	}
+	add_two_newlines();
 	++list_depth;
+	list_levels[list_depth - 1].type = UNORDERED_LIST;
 }
 
 static void
 ul_end_handler(void)
 {
-	heading_end_handler();
-	--list_depth;
+	add_two_newlines();
+	if (list_depth > 0) {
+		--list_depth;
+	}
 }
 
 static void
 ol_start_handler(void)
 {
-	heading_end_handler();
-	current_list_type = ORDERED_LIST;
+	if (list_depth == MAX_NESTED_LISTS_DEPTH) {
+		return;
+	}
+	add_two_newlines();
 	++list_depth;
-	lists_length[list_depth - 1] = 0;
-}
-
-static void
-ol_end_handler(void)
-{
-	heading_end_handler();
-	current_list_type = UNORDERED_LIST;
-	--list_depth;
+	list_levels[list_depth - 1].type = ORDERED_LIST;
+	list_levels[list_depth - 1].length = 0;
 }
 
 static void
@@ -284,7 +281,6 @@ format_text(char *tag, size_t tag_len)
 	strcat(text, tag);
 	text_len += tag_len;
 	text[text_len++] = '>';
-	text[text_len] = '\0';
 }
 
 struct string *
@@ -294,19 +290,27 @@ plainify_html(const char *str, size_t str_len)
 	if (text == NULL) {
 		return NULL;
 	}
+	tag = malloc(sizeof(char) * INITIAL_TAG_BUFFER_SIZE);
+	if (tag == NULL) {
+		free(text);
+		return NULL;
+	}
 	text_len = 0;
-	position = HTML_NONE;
+	tag_lim = INITIAL_TAG_BUFFER_SIZE - 1;
 
 	list_depth = 0;
-	bool   in_tag = false; // shows if str[i] character belongs to html tag
-	bool   error = false;  // shows if error occurred in loop
+	position = HTML_NONE;
+
+	bool in_tag = false; // shows if str[i] character belongs to html tag
+	bool error = false;  // shows if error occurred in loop
 
 	for (size_t i = 0; i < str_len; ++i) {
 		if (in_tag == false) {
 			if (str[i] == '<') {
 				in_tag = true;
 				tag_len = 0;
-			} else if (((position & HTML_STYLE) != 0) || ((position & HTML_SCRIPT) != 0)) {
+			} else if ((position & (HTML_STYLE|HTML_SCRIPT)) != 0) {
+				// Do not write characters of style and script elements.
 				continue;
 			} else if (((position & HTML_PRE) == 0) && ((str[i] == ' ') || (str[i] == '\n') || (str[i] == '\t'))) {
 				if ((text_len != 0) && (text[text_len - 1] != ' ') && (text[text_len - 1] != '\n') && (text[text_len - 1] != '\t')) {
@@ -321,10 +325,21 @@ plainify_html(const char *str, size_t str_len)
 				tag[tag_len] = '\0';
 				format_text(tag, tag_len);
 			} else {
+				if (tag_len == tag_lim) {
+					tag_lim *= 2;
+					temp_ptr = realloc(tag, sizeof(char) * (tag_lim + 1));
+					if (temp_ptr == NULL) {
+						error = true;
+						break;
+					}
+					tag = temp_ptr;
+				}
 				tag[tag_len++] = str[i];
 			}
 		}
 	}
+
+	free(tag);
 
 	if (error == true) {
 		free(text);

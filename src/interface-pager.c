ce39d0f3fd144c9d8812979b81d4defdb26a95c9
Grigory Kirillov <txgk@bk.ru>
2022-05-20 09:56:30 +0300

    get rid of static variables in interface-pager.c

--- src/interface-pager.c
+++ src/interface-pager.c
@@ -1,85 +1,124 @@
 #include <stdlib.h>
 #include "feedeater.h"
 
-// Here, a pager is such a thing that serves as a text viewer.
-
-static WINDOW *window;
-static size_t view_min; // index of first visible line (row)
-static size_t view_lim; // maximum reachable value of view_min
-static size_t pad_height;
+struct pager_menu {
+	WINDOW *window;
+	size_t view_min; // index of first visible line (row)
+	size_t view_lim; // maximum reachable value of view_min
+};
 
 static inline void
-write_splitted_wstring_to_pad(WINDOW *pad, const struct wstring *wbuf)
+write_splitted_wstring_to_window(WINDOW *window, const struct wstring *wbuf)
 {
-	size_t newlines = 0;
+	size_t newlines_count = 0;
 	const wchar_t *iter = wbuf->ptr;
 	const wchar_t *newline_char = wcschr(iter, L'\n');
 	while (newline_char != NULL) {
-		wmove(pad, newlines++, 0);
-		waddnwstr(pad, iter, newline_char - iter);
+		wmove(window, newlines_count++, 0);
+		waddnwstr(window, iter, newline_char - iter);
 		iter = newline_char + 1;
 		newline_char = wcschr(iter, L'\n');
 	}
-	INFO("Wrote %zu lines to pad window.", newlines);
+	INFO("Number of lines written to the window: %zu.", newlines_count);
 }
 
-static WINDOW *
-create_window_with_contents(const struct render_block *data_list)
+static bool
+update_pager_menu(struct pager_menu *menu, const struct render_block *data_list)
 {
 	struct wstring *text = render_data(data_list);
 	if (text == NULL) {
-		status_write("Can not create window for items contents!");
-		FAIL("Render of item data failed!\n");
-		return NULL;
+		FAIL("Failed to render content!");
+		status_write("Can't render content!");
+		return false;
 	}
 	if (text->len == 0) {
-		status_write("Item contents is empty!");
-		WARN("Item contents is empty!");
+		WARN("Rendered content is empty!");
+		status_write("Content is empty!");
 		free_wstring(text);
-		return NULL;
+		return false;
 	}
 
 	// As long as rendered data was generated according to size of the terminal and
 	// every line in it is terminated with newline, we can get required height for
 	// pad window by counting newline characters in the rendered text wstring.
-	pad_height = 0;
+	size_t pad_height = 0;
 	for (size_t i = 0; i < text->len; ++i) {
 		if (text->ptr[i] == L'\n') {
 			++pad_height;
 		}
 	}
 
-	INFO("Creating pad window with %zu width and %zu height.", list_menu_width, pad_height);
-	WINDOW *pad = newpad(pad_height, list_menu_width);
-	if (pad == NULL) {
-		FAIL("Failed to create pad window for item contents (newpad returned NULL)!");
-		free_wstring(text);
-		return NULL;
+	if (menu->window != NULL) {
+		delwin(menu->window);
 	}
 
-	write_splitted_wstring_to_pad(pad, text);
+	INFO("Creating pad window with %zu width and %zu height.", list_menu_width, pad_height);
+	menu->window = newpad(pad_height, list_menu_width);
+	if (menu->window == NULL) {
+		FAIL("Failed to create pad window for item contents (newpad returned NULL)!");
+		free_wstring(text);
+		return false;
+	}
 
-	view_min = 0;
-	view_lim = pad_height > list_menu_height ? pad_height - list_menu_height : 0;
+	write_splitted_wstring_to_window(menu->window, text);
+
+	menu->view_min = 0;
+	menu->view_lim = pad_height > list_menu_height ? pad_height - list_menu_height : 0;
 
 	clear();
 	refresh();
 	status_update();
-	prefresh(pad, view_min, 0, 0, 0, list_menu_height - 1, list_menu_width - 1);
+	prefresh(menu->window, menu->view_min, 0, 0, 0, list_menu_height - 1, list_menu_width - 1);
 
 	free_wstring(text);
 
-	return pad;
+	return true;
 }
 
 static void
-scroll_view(size_t pminrow)
+scroll_view(struct pager_menu *menu, size_t pminrow)
 {
-	if (pminrow == view_min) {
+	if (pminrow == menu->view_min) {
 		return;
 	}
-	view_min = pminrow;
-	prefresh(window, pminrow, 0, 0, 0, list_menu_height - 1, list_menu_width - 1);
+	menu->view_min = pminrow;
+	prefresh(menu->window, pminrow, 0, 0, 0, list_menu_height - 1, list_menu_width - 1);
+}
+
+static inline void
+scroll_one_line_down(struct pager_menu *menu)
+{
+	scroll_view(menu, menu->view_min < menu->view_lim ? menu->view_min + 1 : menu->view_lim);
+}
+
+static inline void
+scroll_one_line_up(struct pager_menu *menu)
+{
+	scroll_view(menu, menu->view_min == 0 ? 0 : menu->view_min - 1);
+}
+
+static inline void
+scroll_one_page_down(struct pager_menu *menu)
+{
+	scroll_view(menu, menu->view_min + list_menu_height < menu->view_lim ? menu->view_min + list_menu_height : menu->view_lim);
+}
+
+static inline void
+scroll_one_page_up(struct pager_menu *menu)
+{
+	scroll_view(menu, menu->view_min > list_menu_height ? menu->view_min - list_menu_height : 0);
+}
+
+static inline void
+scroll_to_the_beginning(struct pager_menu *menu)
+{
+	scroll_view(menu, 0);
+}
+
+static inline void
+scroll_to_the_end(struct pager_menu *menu)
+{
+	scroll_view(menu, menu->view_lim);
 }
 
 // On success - exit by user - returns INPUT_QUIT_SOFT or INPUT_QUIT_HARD.
@@ -87,9 +126,9 @@ scroll_view(size_t pminrow)
 int
 pager_view(const struct render_block *first_block)
 {
-	window = create_window_with_contents(first_block);
-	if (window == NULL) {
-		// Error message written by create_window_with_contents.
+	struct pager_menu menu = {NULL, 0, 0};
+	if (update_pager_menu(&menu, first_block) == false) {
+		// Error message written by update_pager_menu.
 		return INPUTS_COUNT;
 	}
 
@@ -99,21 +138,20 @@ pager_view(const struct render_block *first_block)
 	while (true) {
 		cmd = get_input_command();
 		if ((cmd == INPUT_SELECT_NEXT) || (cmd == INPUT_ENTER)) {
-			scroll_view(view_min < view_lim ? view_min + 1 : view_lim);
+			scroll_one_line_down(&menu);
 		} else if (cmd == INPUT_SELECT_PREV) {
-			scroll_view(view_min == 0 ? 0 : view_min - 1);
+			scroll_one_line_up(&menu);
 		} else if (cmd == INPUT_SELECT_NEXT_PAGE) {
-			scroll_view(view_min + list_menu_height < view_lim ? view_min + list_menu_height : view_lim);
+			scroll_one_page_down(&menu);
 		} else if (cmd == INPUT_SELECT_PREV_PAGE) {
-			scroll_view(view_min > list_menu_height ? view_min - list_menu_height : 0);
+			scroll_one_page_up(&menu);
 		} else if (cmd == INPUT_SELECT_FIRST) {
-			scroll_view(0);
+			scroll_to_the_beginning(&menu);
 		} else if (cmd == INPUT_SELECT_LAST) {
-			scroll_view(view_lim);
+			scroll_to_the_end(&menu);
 		} else if (cmd == INPUT_RESIZE) {
-			delwin(window);
-			window = create_window_with_contents(first_block);
-			if (window == NULL) {
+			if (update_pager_menu(&menu, first_block) == false) {
+				// Error message written by update_pager_menu.
 				return INPUTS_COUNT;
 			}
 		} else if ((cmd == INPUT_QUIT_SOFT) || (cmd == INPUT_QUIT_HARD)) {
@@ -121,7 +159,7 @@ pager_view(const struct render_block *first_block)
 		}
 	}
 
-	delwin(window);
+	delwin(menu.window);
 
 	return cmd;
 }

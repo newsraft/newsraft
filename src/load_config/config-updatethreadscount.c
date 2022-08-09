1d951f9d84f103d71962d49be98398757e45441d
Grigory Kirillov <txgk@bk.ru>
2022-08-09 21:03:05 +0300

    determine the number of processors in a more portable way

--- src/load_config/config-updatethreadscount.c
+++ src/load_config/config-updatethreadscount.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "load_config/load_config.h"
 
 void
 set_sane_value_for_update_threads_count(size_t initial_value)
 {
-	FILE *p = popen("nproc", "r"); // prints CPUs count
-	if (p == NULL) {
+	long online_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+	INFO("Sysconf value of _SC_NPROCESSORS_ONLN equals to %ld.", online_cpus);
+	if (online_cpus <= 1) {
 		set_cfg_uint(CFG_UPDATE_THREADS_COUNT, 1);
+	} else if (initial_value == 0) {
+		set_cfg_uint(CFG_UPDATE_THREADS_COUNT, 2);
 	} else {
-		size_t nproc_output;
-		if (fscanf(p, "%zu", &nproc_output) != 1) {
-			set_cfg_uint(CFG_UPDATE_THREADS_COUNT, 1);
-		} else {
-			if (initial_value == 0) {
-				set_cfg_uint(CFG_UPDATE_THREADS_COUNT, nproc_output > 1 ? 2 : 1);
-			} else {
-				set_cfg_uint(CFG_UPDATE_THREADS_COUNT, MIN(initial_value, nproc_output));
-			}
-		}
-		pclose(p);
+		set_cfg_uint(CFG_UPDATE_THREADS_COUNT, MIN(initial_value, (size_t)online_cpus));
 	}
 }

886e495c352255ef8bd64cdf05a152068c8ff7c4
Grigory Kirillov <txgk@bk.ru>
2022-09-13 08:36:42 +0300

    improve html table renderer

--- src/render_data/render-text-html-table.c
+++ src/render_data/render-text-html-table.c
@@ -3,13 +3,12 @@
 #include <string.h>
 #include "render_data/render_data.h"
 
-// This HTML table renderer needs to be rewritten because it's kind of creepy :|
-
 struct html_table_cell {
 	struct wstring **lines;
-	int64_t lines_count;
+	size_t lines_count;
 	int8_t colspan;
 	int8_t rowspan;
+	int64_t index;
 };
 
 struct html_table_row {
@@ -19,11 +18,11 @@ struct html_table_row {
 
 struct html_table {
 	struct html_table_row *rows;
-	int64_t rows_count;            // number of rows in table
-	int64_t columns_count;         // max cells_count of rows
-	int64_t *column_widths;        // max widths of cell per column
+	int64_t rows_count;
+	int64_t columns_count;
+	size_t *column_widths;
 	struct wstring *unmapped_text; // text outside of cells
-	struct wstring *target;
+	struct html_table_cell *target;
 };
 
 struct html_table_element_handler {
@@ -33,59 +32,26 @@ struct html_table_element_handler {
 };
 
 static void
-append_empty_cell_to_last_row_of_the_table(struct html_table *table)
-{
-	table->rows[table->rows_count - 1].cells_count += 1;
-	struct html_table_cell *tmp = realloc(
-		table->rows[table->rows_count - 1].cells,
-		sizeof(struct html_table_cell) * table->rows[table->rows_count - 1].cells_count
-	);
-	if (tmp == NULL) {
-		return;
-	}
-	tmp[table->rows[table->rows_count - 1].cells_count - 1].lines = malloc(sizeof(struct wstring *));
-	tmp[table->rows[table->rows_count - 1].cells_count - 1].lines_count = 1;
-	tmp[table->rows[table->rows_count - 1].cells_count - 1].lines[0] = wcrtes();
-	tmp[table->rows[table->rows_count - 1].cells_count - 1].colspan = 1;
-	tmp[table->rows[table->rows_count - 1].cells_count - 1].rowspan = 1;
-	table->rows[table->rows_count - 1].cells = tmp;
-}
-
-static void
 expand_html_table_by_one_row(struct html_table *table, GumboVector *attrs)
 {
 	(void)attrs;
-	struct html_table_row *temp = realloc(table->rows, sizeof(struct html_table_row) * (table->rows_count + 1));
-	if (temp != NULL) {
-		temp[table->rows_count].cells = NULL;
-		temp[table->rows_count].cells_count = 0;
-		table->rows = temp;
+	struct html_table_row *tmp = realloc(table->rows, sizeof(struct html_table_row) * (table->rows_count + 1));
+	if (tmp != NULL) {
+		tmp[table->rows_count].cells = NULL;
+		tmp[table->rows_count].cells_count = 0;
+		table->rows = tmp;
 		table->rows_count += 1;
-	}
-	// Populate new row with empty columns that we get from rowspan of columns from previous rows.
-	// Note that we do it only for the first consecutive columns with rowspan attribute.
-	for (int64_t i = 0; i < table->rows_count - 1; ++i) {
-		for (int64_t j = 0; j < table->rows[i].cells_count; ++j) {
-			if (table->rows[i].cells[j].rowspan == 1) {
-				break;
-			}
-			if (table->rows[i].cells[j].rowspan - table->rows_count + i >= 0) {
-				append_empty_cell_to_last_row_of_the_table(table);
-			}
-		}
 	}
 }
 
 static int8_t
-get_int8_t_of_xml_attribute(GumboVector *attrs, const char *attr_name)
+get_cells_count_from_tag_attribute(GumboVector *attrs, const char *attr_name)
 {
 	GumboAttribute *attr = gumbo_get_attribute(attrs, attr_name);
-	if (attr != NULL) {
-		if (attr->value != NULL) {
-			int8_t result;
-			if (sscanf(attr->value, "%" SCNd8, &result) == 1) {
-				return result;
-			}
+	if ((attr != NULL) && (attr->value != NULL)) {
+		int8_t result;
+		if (sscanf(attr->value, "%" SCNd8, &result) == 1) {
+			return result > 0 ? result : 1;
 		}
 	}
 	return 1;
@@ -101,55 +67,66 @@ expand_last_row_in_html_table_by_one_cell(struct html_table *table, GumboVector 
 		}
 	}
 
-	bool filled_reserved_space = false;
-	int64_t index;
+	int64_t i;
+	struct html_table_row *last_row = &table->rows[table->rows_count - 1];
+	int64_t old_count = last_row->cells_count;
+
 	// Before adding another cell we have to check if the place where we write
 	// the cell to is not reserved by the cells with the rowspan attribute
-	// above. If it is reserved, we have to iteratively fill reserved space
-	// with empty cells.
-	while (filled_reserved_space == false) {
-		filled_reserved_space = true;
-		index = table->rows[table->rows_count - 1].cells_count;
-		for (int64_t i = 0; i < table->rows_count - 1; ++i) {
-			if (index < table->rows[i].cells_count) {
-				if (table->rows[i].cells[index].rowspan - table->rows_count + i >= 0) {
-					filled_reserved_space = false;
-					append_empty_cell_to_last_row_of_the_table(table);
-				}
+	// above. If it is reserved, we have to shift write target to another cell.
+	for (i = 0; i < table->rows_count - 1; ++i) {
+		if (last_row->cells_count < table->rows[i].cells_count) {
+			if (table->rows[i].cells[last_row->cells_count].rowspan + i >= table->rows_count) {
+				last_row->cells_count += 1;
+				i = -1; // And again.
 			}
 		}
 	}
 
-	int8_t colspan = get_int8_t_of_xml_attribute(attrs, "colspan");
-	struct html_table_cell *temp = realloc(table->rows[table->rows_count - 1].cells, sizeof(struct html_table_cell) * (index + colspan));
-	if (temp == NULL) {
+	int8_t colspan = get_cells_count_from_tag_attribute(attrs, "colspan");
+	int8_t rowspan = get_cells_count_from_tag_attribute(attrs, "rowspan");
+
+	last_row->cells_count += colspan;
+
+	struct html_table_cell *tmp = realloc(last_row->cells, sizeof(struct html_table_cell) * last_row->cells_count);
+	if (tmp == NULL) {
 		return;
 	}
-	int8_t rowspan = get_int8_t_of_xml_attribute(attrs, "rowspan");
-	for (int64_t i = index + 1; i < (index + colspan); ++i) {
-		temp[i].lines = NULL;
-		temp[i].colspan = 1;
-		temp[i].rowspan = rowspan;
+
+	for (i = old_count; i < (last_row->cells_count - colspan); ++i) {
+		tmp[i].lines = NULL;
+		tmp[i].colspan = 1; // TODO borrow colspan from cells above
+		tmp[i].rowspan = -1;
+		tmp[i].index = i;
 	}
-	temp[index].lines = malloc(sizeof(struct wstring *));
-	temp[index].lines[0] = wcrtes();
-	temp[index].lines_count = 1;
-	temp[index].colspan = colspan;
-	temp[index].rowspan = rowspan;
-	table->rows[table->rows_count - 1].cells = temp;
-	table->rows[table->rows_count - 1].cells_count += colspan;
-	if (table->rows[table->rows_count - 1].cells_count > table->columns_count) {
+	for (i = last_row->cells_count - colspan + 1; i < last_row->cells_count; ++i) {
+		tmp[i].lines = NULL;
+		tmp[i].colspan = -1;
+		tmp[i].rowspan = rowspan;
+		tmp[i].index = i;
+	}
+
+	tmp[last_row->cells_count - colspan].lines = malloc(sizeof(struct wstring *));
+	tmp[last_row->cells_count - colspan].lines[0] = wcrtes();
+	tmp[last_row->cells_count - colspan].lines_count = 1;
+	tmp[last_row->cells_count - colspan].colspan = colspan;
+	tmp[last_row->cells_count - colspan].rowspan = rowspan;
+	tmp[last_row->cells_count - colspan].index = last_row->cells_count - colspan;
+	last_row->cells = tmp;
+
+	if (last_row->cells_count > table->columns_count) {
 		int64_t prev_columns_count = table->columns_count;
-		table->columns_count = table->rows[table->rows_count - 1].cells_count;
-		int64_t *temp2 = realloc(table->column_widths, sizeof(int64_t) * table->columns_count);
-		if (temp2 != NULL) {
-			table->column_widths = temp2;
-			for (int64_t i = prev_columns_count; i < table->columns_count; ++i) {
+		table->columns_count = last_row->cells_count;
+		size_t *tmp2 = realloc(table->column_widths, sizeof(int64_t) * table->columns_count);
+		if (tmp2 != NULL) {
+			table->column_widths = tmp2;
+			for (i = prev_columns_count; i < table->columns_count; ++i) {
 				table->column_widths[i] = 0;
 			}
 		}
 	}
-	table->target = temp[index].lines[0];
+
+	table->target = &tmp[last_row->cells_count - colspan];
 }
 
 static void
@@ -159,29 +136,28 @@ write_to_table(struct html_table *table, const char *src, size_t src_len)
 	if (tag == NULL) {
 		return;
 	}
+	inlinefy_string(tag);
 	struct wstring *wtag = convert_string_to_wstring(tag);
 	free_string(tag);
 	if (wtag == NULL) {
 		return;
 	}
-	if (wcatss(table->target, wtag) == false) {
-		free_wstring(wtag);
-		return;
+	if (table->target != NULL) {
+		struct wstring *last_line = table->target->lines[table->target->lines_count - 1];
+		wcatss(last_line, wtag);
+		// Don't expand width of an entire column just because of the content
+		// of a cell that spans multiple columns. The required width will be
+		// calculated later to evenly space the content of cells that are not
+		// spread across multiple columns.
+		if (table->target->colspan == 1) {
+			if (table->column_widths[table->target->index] < last_line->len) {
+				table->column_widths[table->target->index] = last_line->len;
+			}
+		}
+	} else {
+		wcatss(table->unmapped_text, wtag);
 	}
 	free_wstring(wtag);
-	if ((table->rows_count > 0) && (table->rows[table->rows_count - 1].cells_count > 0)) {
-		int64_t i;
-		for (i = table->rows[table->rows_count - 1].cells_count - 1; i > 0; --i) {
-			if (table->rows[table->rows_count - 1].cells[i].lines != NULL) {
-				break;
-			}
-		}
-		if (table->rows[table->rows_count - 1].cells[i].colspan == 1) {
-			if ((size_t)(table->column_widths[i]) < table->target->len) {
-				table->column_widths[i] = (int64_t)(table->target->len);
-			}
-		}
-	}
 }
 
 static inline void
@@ -190,9 +166,11 @@ free_html_table(const struct html_table *table)
 	for (int64_t i = 0; i < table->rows_count; ++i) {
 		for (int64_t j = 0; j < table->rows[i].cells_count; ++j) {
 			if (table->rows[i].cells[j].lines != NULL) {
-				free_wstring(table->rows[i].cells[j].lines[0]);
-				free(table->rows[i].cells[j].lines);
+				for (size_t k = 0; k < table->rows[i].cells[j].lines_count; ++k) {
+					free_wstring(table->rows[i].cells[j].lines[k]);
+				}
 			}
+			free(table->rows[i].cells[j].lines);
 		}
 		free(table->rows[i].cells);
 	}
@@ -204,21 +182,23 @@ free_html_table(const struct html_table *table)
 static inline void
 adjust_column_widths_to_contain_cells_with_colspan_attribute(struct html_table *table)
 {
-	int64_t sum_width;
-	int64_t *min_width;
+	size_t sum_width;
+	size_t *min_width;
 	for (int64_t i = 0; i < table->rows_count; ++i) {
 		for (int64_t j = 0; j < table->rows[i].cells_count; ++j) {
 			if (table->rows[i].cells[j].colspan > 1) {
 				while (true) {
-					sum_width = 0;
+					sum_width = table->column_widths[j];
 					min_width = &table->column_widths[j];
-					for (int64_t k = 0; (k < table->rows[i].cells[j].colspan) && ((j + k) < table->columns_count); ++k) {
+					for (int64_t k = 1; (k < table->rows[i].cells[j].colspan) && ((j + k) < table->columns_count); ++k) {
+						sum_width += table->column_widths[j + k];
 						if (table->column_widths[j + k] < *min_width) {
 							min_width = &table->column_widths[j + k];
 						}
-						sum_width += table->column_widths[j + k];
 					}
-					if ((size_t)sum_width < table->rows[i].cells[j].lines[0]->len) {
+					// table->rows[i].cells[j].lines is not NULL because
+					// all cells with (colspan > 1) have at least one line.
+					if (sum_width < table->rows[i].cells[j].lines[0]->len) {
 						*min_width += 1;
 					} else {
 						break;
@@ -232,7 +212,7 @@ adjust_column_widths_to_contain_cells_with_colspan_attribute(struct html_table *
 static inline void
 print_html_table(struct wstring *text, struct line *line, const struct html_table *table)
 {
-	wchar_t buf[5000];
+	wchar_t buf[8192];
 	size_t w;
 	if (table->unmapped_text->len > 0) {
 		line_string(line, table->unmapped_text->ptr, text);
@@ -240,14 +220,16 @@ print_html_table(struct wstring *text, struct line *line, const struct html_tabl
 	}
 	for (int64_t i = 0; i < table->rows_count; ++i) {
 		for (int64_t j = 0; j < table->rows[i].cells_count; ++j) {
-			if (table->rows[i].cells[j].lines != NULL) {
+			if (table->rows[i].cells[j].colspan > 0) {
 				w = table->column_widths[j];
-				if (table->rows[i].cells[j].colspan > 1) {
-					for (int64_t k = j + 1; (k < table->rows[i].cells_count) && (table->rows[i].cells[k].lines == NULL); ++k) {
-						w += table->column_widths[k] + 1;
-					}
+				for (int64_t k = j + 1; (k < table->rows[i].cells_count) && (table->rows[i].cells[k].colspan == -1); ++k) {
+					w += table->column_widths[k] + 1;
 				}
-				swprintf(buf, 5000, L"%-*.*ls ", w, w, table->rows[i].cells[j].lines[0]->ptr);
+				if (table->rows[i].cells[j].rowspan == -1) {
+					swprintf(buf, 8192, L"%-*.*ls ", w, w, L"");
+				} else {
+					swprintf(buf, 8192, L"%-*.*ls ", w, w, table->rows[i].cells[j].lines[0]->ptr);
+				}
 				line_string(line, buf, text);
 			}
 		}
@@ -271,24 +253,22 @@ static void
 dump_html_table_child(GumboNode *node, struct html_table *table)
 {
 	if (node->type == GUMBO_NODE_ELEMENT) {
-		size_t i, j;
-		for (i = 0; handlers[i].tag_id != GUMBO_TAG_UNKNOWN; ++i) {
-			if (node->v.element.tag == handlers[i].tag_id) {
-				break;
-			}
+		size_t i = 0, j = 0;
+		while ((node->v.element.tag != handlers[i].tag_id) && (handlers[i].tag_id != GUMBO_TAG_UNKNOWN)) {
+			i += 1;
 		}
 		if (handlers[i].tag_id == GUMBO_TAG_UNKNOWN) {
 			write_to_table(table, node->v.element.original_tag.data, node->v.element.original_tag.length);
-			for (j = 0; j < node->v.element.children.length; ++j) {
-				dump_html_table_child(node->v.element.children.data[j], table);
+			while (j < node->v.element.children.length) {
+				dump_html_table_child(node->v.element.children.data[j++], table);
 			}
 			write_to_table(table, node->v.element.original_end_tag.data, node->v.element.original_end_tag.length);
 		} else {
 			if (handlers[i].start_handler != NULL) {
 				handlers[i].start_handler(table, &node->v.element.attributes);
 			}
-			for (j = 0; j < node->v.element.children.length; ++j) {
-				dump_html_table_child(node->v.element.children.data[j], table);
+			while (j < node->v.element.children.length) {
+				dump_html_table_child(node->v.element.children.data[j++], table);
 			}
 			if (handlers[i].end_handler != NULL) {
 				handlers[i].end_handler(table, &node->v.element.attributes);
@@ -307,7 +287,6 @@ write_contents_of_html_table_node_to_text(struct wstring *text, struct line *lin
 	if (table.unmapped_text == NULL) {
 		return;
 	}
-	table.target = table.unmapped_text;
 	for (size_t i = 0; i < node->v.element.children.length; ++i) {
 		dump_html_table_child(node->v.element.children.data[i], &table);
 	}

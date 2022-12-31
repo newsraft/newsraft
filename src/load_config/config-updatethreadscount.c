867430e7edca95348351b11bd2e50a37d911180b
Grigory Kirillov <txgk@bk.ru>
2023-01-01 01:50:45 +0300

    use nproc command to determine cpus count if _SC_NPROCESSORS_ONLN macro is not defined

--- src/load_config/config-updatethreadscount.c
+++ src/load_config/config-updatethreadscount.c
@@ -4,8 +4,18 @@
 void
 set_sane_value_for_update_threads_count(size_t initial_value)
 {
+#ifdef _SC_NPROCESSORS_ONLN
 	long online_cpus = sysconf(_SC_NPROCESSORS_ONLN);
 	INFO("Sysconf value of _SC_NPROCESSORS_ONLN equals to %ld.", online_cpus);
+#else
+	size_t online_cpus = 2;
+	FILE *p = popen("nproc", "r"); // Prints processors count.
+	if (p != NULL) {
+		if (fscanf(p, "%zu", &online_cpus) == 1) {
+			INFO("Output of nproc command is %zu.", online_cpus);
+		}
+	}
+#endif
 	if (online_cpus <= 1) {
 		set_cfg_uint(CFG_UPDATE_THREADS_COUNT, 1);
 	} else if (initial_value == 0) {

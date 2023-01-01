1a794bba5b6f9f983b080a259ee58e15fff403b4
Grigory Kirillov <txgk@bk.ru>
2023-01-01 14:57:14 +0300

    read cpu cores count from sysctl -n hw.logicalcpu command on Apple machines

--- src/load_config/config-updatethreadscount.c
+++ src/load_config/config-updatethreadscount.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
 #include "load_config/load_config.h"
 
+#ifdef __APPLE__
+#define CPU_CORES_COUNT_COMMAND "sysctl -n hw.logicalcpu"
+#else
+#define CPU_CORES_COUNT_COMMAND "nproc"
+#endif
+
 void
 set_sane_value_for_update_threads_count(size_t initial_value)
 {
@@ -9,7 +15,7 @@ set_sane_value_for_update_threads_count(size_t initial_value)
 	INFO("Sysconf value of _SC_NPROCESSORS_ONLN equals to %ld.", online_cpus);
 #else
 	size_t online_cpus = 2;
-	FILE *p = popen("nproc", "r"); // Prints processors count.
+	FILE *p = popen(CPU_CORES_COUNT_COMMAND, "r"); // Prints processors count.
 	if (p != NULL) {
 		if (fscanf(p, "%zu", &online_cpus) == 1) {
 			INFO("Output of nproc command is %zu.", online_cpus);

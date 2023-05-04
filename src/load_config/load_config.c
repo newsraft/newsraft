4144a1d489597d0d7ca0ff9cce00c4c225e083d3
Grigory Kirillov <txgk@bk.ru>
2023-05-04 20:33:22 +0300

    obtain number of cpu cores in even more portable way

--- src/load_config/load_config.c
+++ src/load_config/load_config.c
@@ -1,12 +1,35 @@
-#include <unistd.h>
 #include "load_config/load_config.h"
+
+static size_t
+get_number_of_processors(void)
+{
+	const char *cmds[] = {
+		"nproc 2>/dev/null",
+		"sysctl -n hw.ncpu 2>/dev/null",
+		"getconf _NPROCESSORS_ONLN 2>/dev/null",
+		"grep -c ^processor /proc/cpuinfo 2>/dev/null",
+		NULL,
+	};
+	size_t cpus = 0;
+	for (const char **i = cmds; i != NULL; ++i) {
+		FILE *p = popen(*i, "r");
+		if (p != NULL) {
+			fscanf(p, "%zu", &cpus);
+			INFO("Number of processors from %s is %zu", *i, cpus);
+			pclose(p);
+		}
+		if (cpus > 0) {
+			return cpus;
+		}
+	}
+	return 0;
+}
 
 bool
 load_config(void)
 {
 	const size_t preferred_threads = get_cfg_uint(CFG_UPDATE_THREADS_COUNT);
-	const long online_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-	INFO("Sysconf value of _SC_NPROCESSORS_ONLN is %ld", online_cpus);
+	const size_t online_cpus = get_number_of_processors();
 	const size_t real_threads = online_cpus > 1 ? online_cpus : 1;
 	if (preferred_threads == 0) {
 		set_cfg_uint(CFG_UPDATE_THREADS_COUNT, real_threads);

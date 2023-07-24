057464b85eb2c51d6a37ba01b12b8841e84f88f5
Grigory Kirillov <txgk@bk.ru>
2023-07-24 10:21:39 +0300

    add gopher to valid protocol schemes

--- src/feeds-parse.c
+++ src/feeds-parse.c
@@ -16,11 +16,12 @@ check_url_for_validity(const struct string *url)
 	if ((strncmp(url->ptr, "http://", 7) != 0)
 		&& (strncmp(url->ptr, "https://", 8) != 0)
 		&& (strncmp(url->ptr, "ftp://", 6) != 0)
+		&& (strncmp(url->ptr, "gopher://", 9) != 0)
 		&& (strncmp(url->ptr, "file://", 7) != 0))
 	{
 		fprintf(stderr, "Stumbled across an invalid URL: \"%s\"!\n", url->ptr);
 		fputs("Every feed URL must start with a protocol scheme like \"http://\".\n", stderr);
-		fputs("Supported protocol schemes are http, https, ftp and file.\n", stderr);
+		fputs("Supported protocol schemes are http, https, ftp, gopher and file.\n", stderr);
 		return false;
 	}
 	return true;

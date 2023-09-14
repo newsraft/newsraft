3b0379e89d28575229b1b8502d7ff0f0514b8330
Grigory Kirillov <txgk@bk.ru>
2023-09-14 10:09:54 +0300

    improve trim_whitespace_from_string test

--- tests/trim_whitespace_from_string.c
+++ tests/trim_whitespace_from_string.c
@@ -1,34 +1,30 @@
 #include <string.h>
 #include "newsraft.h"
 
+static const char *test_cases[][2] = {
+	{"The Devil's Advocate", "The Devil's Advocate"},
+	{"Monsters,   Inc.",     "Monsters,   Inc."},
+	{"The    Terminal",      "The    Terminal"},
+	{"消失的她",             "     消失的她"},
+	{"The Departed",         "The Departed           "},
+	{"影",                   "\t \n \t 影 \t \r \t"},
+	{"Black Swan",           "       Black Swan     "},
+	{"El hoyo",              " El hoyo "},
+	{NULL,                   NULL},
+};
+
 int
 main(void)
 {
 	struct string *str = crtes(100);
-
-	cpyas(str, "The Devil's Advocate", 20);
-	trim_whitespace_from_string(str);
-	if (strcmp(str->ptr, "The Devil's Advocate") != 0) goto error;
-
-	cpyas(str, "      The Terminal", 18);
-	trim_whitespace_from_string(str);
-	if (strcmp(str->ptr, "The Terminal") != 0) goto error;
-
-	cpyas(str, "Black Swan    ", 14);
-	trim_whitespace_from_string(str);
-	if (strcmp(str->ptr, "Black Swan") != 0) goto error;
-
-	cpyas(str, "     El hoyo       ", 19);
-	trim_whitespace_from_string(str);
-	if (strcmp(str->ptr, "El hoyo") != 0) goto error;
-
-	cpyas(str, " The Departed ", 14);
-	trim_whitespace_from_string(str);
-	if (strcmp(str->ptr, "The Departed") != 0) goto error;
-
+	for (size_t i = 0; test_cases[i][0] != NULL; ++i) {
+		cpyas(str, test_cases[i][1], strlen(test_cases[i][1]));
+		trim_whitespace_from_string(str);
+		if (strcmp(str->ptr, test_cases[i][0]) != 0) {
+			free_string(str);
+			return 1;
+		}
+	}
 	free_string(str);
 	return 0;
-error:
-	free_string(str);
-	return 1;
 }

c9a9d1701a5a3927108842486571f430d62a9c73
Grigory Kirillov <txgk@bk.ru>
2023-10-03 03:03:34 +0300

    set version to 0.21

--- src/newsraft.h
+++ src/newsraft.h
@@ -11,7 +11,7 @@
 #include <curses.h>
 
 #ifndef NEWSRAFT_VERSION
-#define NEWSRAFT_VERSION "0.20"
+#define NEWSRAFT_VERSION "0.21"
 #endif
 
 #define ISWHITESPACE(A) (((A)==' ')||((A)=='\n')||((A)=='\t')||((A)=='\v')||((A)=='\f')||((A)=='\r'))

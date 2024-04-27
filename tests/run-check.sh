c08c25979be07967adef82cb28ab39a05ab564d0
Sekulum Forka <dictatorofturkmenistan@protonmail.com>
2024-04-27 18:42:39 +0000

    make simpler use of linking argument in tests/run-check.sh (#96)
    Clang apparently doesn't support the -l: syntax, tested with clang 14
    and 17.
    
    Reviewed-on: https://codeberg.org/newsraft/newsraft/pulls/97
    Co-authored-by: Sekulum Forka <dictatorofturkmenistan@protonmail.com>
    Co-committed-by: Sekulum Forka <dictatorofturkmenistan@protonmail.com>

--- tests/run-check.sh
+++ tests/run-check.sh
@@ -13,7 +13,7 @@ for test_file in *.c
 do
 	tests_count="$((tests_count + 1))"
 	rm -rf test-database*
-	if ${CC:-cc} -Isrc -DTEST -o ./a.out "$test_file" -L. -l:libnewsraft.so && env LD_LIBRARY_PATH=. ./a.out; then
+	if ${CC:-cc} -Isrc -DTEST -o ./a.out "$test_file" -L. -lnewsraft && env LD_LIBRARY_PATH=. ./a.out; then
 		echo "[OKAY] $test_file"
 		okays_count="$((okays_count + 1))"
 	else

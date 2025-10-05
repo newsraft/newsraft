newsraft/newsraft

commit af68a400ba90e1dc54c4ac1ab30519e8a4f1f0b1

store feed urls without trailing slashes in the database (#224)

src/items-list.c
@@ -220,7 +220,7 @@ update_menu_item_list(struct menu_state *ctx)
 		goto undo2;
 	}
 	for (size_t i = 0; i < new_items->feeds_count; ++i) {
-		db_bind_string(new_items->res, i + 1, new_items->feeds[i]->url);
+		db_bind_feed_url(new_items->res, i + 1, new_items->feeds[i]->url);
 	}
 	int search_token_iter = 0;
 	for (const struct menu_state *m = ctx; m != NULL; m = m->prev) {

src/db-items.c
@@ -72,7 +72,7 @@ db_count_items(struct feed_entry **feeds, size_t feeds_count, bool count_only_un
 		goto error;
 	}
 	for (size_t i = 0; i < feeds_count; ++i) {
-		db_bind_string(res, i + 1, feeds[i]->url);
+		db_bind_feed_url(res, i + 1, feeds[i]->url);
 	}
 	if (sqlite3_step(res) == SQLITE_ROW) {
 		count = sqlite3_column_int64(res, 0);
@@ -105,7 +105,7 @@ db_change_unread_status_of_all_items_in_feeds(struct feed_entry **feeds, size_t 
 	}
 	sqlite3_bind_int(res, 1, unread);
 	for (size_t i = 0; i < feeds_count; ++i) {
-		db_bind_string(res, i + 2, feeds[i]->url);
+		db_bind_feed_url(res, i + 2, feeds[i]->url);
 	}
 	bool status = sqlite3_step(res) == SQLITE_DONE ? true : false;
 	sqlite3_finalize(res);

src/db.c
@@ -315,6 +315,29 @@ db_bind_string(sqlite3_stmt *stmt, int pos, const struct string *str)
 	}
 }
 
+// You might wonder why we would ever need a special function to bind a feed URL
+// specifically. Well, that's simple: sometimes user specifies URL with trailing
+// slashes, sometimes without them. These URLs can point to different content in
+// practice, but for the most part it's either
+// * both URLs (with and without trailing slashes) point to the same content, or
+// * one URL points to useful content and the other one is simply empty (404ing)
+//
+// Database-wise, we should treat these URLs as a single entity,
+// so that the user doesn't have to suffer from the fragmentation of the
+// same feed based on the presence or absence of slashes in the URL.
+int
+db_bind_feed_url(sqlite3_stmt *stmt, int pos, const struct string *str)
+{
+	if (STRING_IS_EMPTY(str)) {
+		return sqlite3_bind_null(stmt, pos);
+	}
+	size_t len = str->len;
+	while (len > 0 && str->ptr[len - 1] == '/') {
+		len -= 1;
+	}
+	return sqlite3_bind_text(stmt, pos, str->ptr, len, SQLITE_STATIC);
+}
+
 int64_t
 db_get_date_from_feeds_table(const struct string *url, const char *column, size_t column_len)
 {
@@ -326,7 +349,7 @@ db_get_date_from_feeds_table(const struct string *url, const char *column, size_
 	if (res == NULL) {
 		return -1;
 	}
-	db_bind_string(res, 1, url);
+	db_bind_feed_url(res, 1, url);
 	int64_t date = sqlite3_step(res) == SQLITE_ROW ? sqlite3_column_int64(res, 0) : 0;
 	sqlite3_finalize(res);
 	INFO("%s of %s is %" PRId64, column, url->ptr, date);
@@ -342,7 +365,7 @@ db_get_string_from_feed_table(const struct string *url, const char *column, size
 	memcpy(query + 7 + column_len, " FROM feeds WHERE feed_url=?", 29);
 	sqlite3_stmt *res = db_prepare(query, 7 + column_len + 29, NULL);
 	if (res != NULL) {
-		db_bind_string(res, 1, url);
+		db_bind_feed_url(res, 1, url);
 		if (sqlite3_step(res) == SQLITE_ROW) {
 			const char *str_ptr = (char *)sqlite3_column_text(res, 0);
 			if (str_ptr != NULL) {
@@ -371,7 +394,7 @@ db_update_feed_int64(const struct string *url, const char *column_name, int64_t 
 		sqlite3_stmt *res = db_prepare(query, strlen(query) + 1, NULL);
 		if (res != NULL) {
 			sqlite3_bind_int64(res, 1, value);
-			db_bind_string(res, 2, url);
+			db_bind_feed_url(res, 2, url);
 			if (sqlite3_step(res) != SQLITE_DONE) {
 				FAIL("Failed to update %s!", column_name);
 			}
@@ -392,7 +415,7 @@ db_update_feed_string(const struct string *url, const char *column_name, const s
 		sqlite3_stmt *res = db_prepare(query, strlen(query) + 1, NULL);
 		if (res != NULL) {
 			db_bind_string(res, 1, value);
-			db_bind_string(res, 2, url);
+			db_bind_feed_url(res, 2, url);
 			if (sqlite3_step(res) != SQLITE_DONE) {
 				FAIL("Failed to update %s!", column_name);
 			}
@@ -446,13 +469,13 @@ db_perform_user_edit(const struct wstring *fmt, struct feed_entry **feeds, size_
 	for (size_t i = 0, j = 1; i < replacements_count; ++i) {
 		if (item) {
 			sqlite3_bind_int64(stmt, j++, item->rowid);
-			db_bind_string(stmt, j++, item->feed[0]->url);
+			db_bind_feed_url(stmt, j++, item->feed[0]->url);
 			db_bind_string(stmt, j++, item->guid);
 			continue;
 		}
 		if (feeds && feeds_count > 0) {
 			for (size_t k = 0; k < feeds_count; ++k) {
-				db_bind_string(stmt, j++, feeds[k]->url);
+				db_bind_feed_url(stmt, j++, feeds[k]->url);
 			}
 			continue;
 		}

src/insert_feed/insert-feed-data.c
@@ -7,7 +7,7 @@ insert_feed_data(const struct feed_entry *feed_entry, struct getfeed_feed *feed)
 	if (s == NULL) {
 		return false;
 	}
-	db_bind_string(s,     1 + FEED_COLUMN_FEED_URL,                  feed_entry->url);
+	db_bind_feed_url(s,   1 + FEED_COLUMN_FEED_URL,                  feed_entry->url);
 	db_bind_string(s,     1 + FEED_COLUMN_TITLE,                     feed->title);
 	db_bind_string(s,     1 + FEED_COLUMN_LINK,                      feed->link);
 	db_bind_string(s,     1 + FEED_COLUMN_CONTENT,                   feed->content);

src/insert_feed/insert-item-data.c
@@ -20,7 +20,7 @@ delete_excess_items(struct feed_entry *feed, int64_t limit)
 		FAIL("Failed to prepare an excess items deletion statement!");
 		return false;
 	}
-	db_bind_string(s, 1, feed->url);
+	db_bind_feed_url(s, 1, feed->url);
 	sqlite3_bind_int64(s, 2, limit);
 	sqlite3_step(s);
 	sqlite3_finalize(s);
@@ -46,7 +46,7 @@ db_insert_item(struct feed_entry *feed, struct getfeed_item *item, int64_t rowid
 		return false;
 	}
 
-	db_bind_string(s,     1 + ITEM_COLUMN_FEED_URL,         feed->url);
+	db_bind_feed_url(s,   1 + ITEM_COLUMN_FEED_URL,         feed->url);
 	db_bind_string(s,     1 + ITEM_COLUMN_GUID,             item->guid);
 	db_bind_string(s,     1 + ITEM_COLUMN_TITLE,            item->title);
 	db_bind_string(s,     1 + ITEM_COLUMN_LINK,             STRING_IS_EMPTY(item->link) && item->guid_is_link == true ? item->guid : item->link);
@@ -100,7 +100,7 @@ insert_item_data(struct feed_entry *feed, struct getfeed_item *item)
 		FAIL("Failed to prepare SELECT statement for searching item duplicate by guid!");
 		return false;
 	}
-	db_bind_string(s, 1, feed->url);
+	db_bind_feed_url(s, 1, feed->url);
 	db_bind_string(s, 2, item->guid);
 
 	int64_t item_rowid;

src/newsraft.h
@@ -443,6 +443,7 @@ bool db_transaction_commit(void);
 bool db_rollback_transaction(void);
 const char *db_error_string(void);
 int db_bind_string(sqlite3_stmt *stmt, int pos, const struct string *str);
+int db_bind_feed_url(sqlite3_stmt *stmt, int pos, const struct string *str);
 int64_t db_get_date_from_feeds_table(const struct string *url, const char *column, size_t column_len);
 struct string *db_get_string_from_feed_table(const struct string *url, const char *column, size_t column_len);
 void db_update_feed_int64(const struct string *url, const char *column_name, int64_t value, bool only_positive);

src/sections.c
@@ -225,7 +225,7 @@ purge_abandoned_feeds(void)
 		return false;
 	}
 	for (size_t i = 0; i < sections[0].feeds_count; ++i) {
-		db_bind_string(res, i + 1, sections[0].feeds[i]->url);
+		db_bind_feed_url(res, i + 1, sections[0].feeds[i]->url);
 	}
 	bool status = sqlite3_step(res) == SQLITE_DONE ? true : false;
 	sqlite3_finalize(res);

commit 8933b0b9ac3211d6980cdf68dd0c349b0b74a0c6

fix terrible oversight on my part introduced in af68a400ba (#236)

src/items-list.c
@@ -110,8 +110,15 @@ static inline struct feed_entry **
 find_feed_entry_by_url(struct feed_entry **feeds, size_t feeds_count, const char *feed_url)
 {
 	if (feed_url != NULL) {
+		size_t feed_url_len = strlen(feed_url);
 		for (size_t i = 0; i < feeds_count; ++i) {
-			if (strcmp(feed_url, feeds[i]->url->ptr) == 0) {
+			// Feed URLs in the database are stored without trailing slashes.
+			// To make the search legit we have to strip slashes from the user's URLs also.
+			size_t clean_len = feeds[i]->url->len;
+			while (clean_len > 0 && feeds[i]->url->ptr[clean_len - 1] == '/') {
+				clean_len -= 1;
+			}
+			if (feed_url_len == clean_len && strncmp(feed_url, feeds[i]->url->ptr, clean_len) == 0) {
 				return feeds + i;
 			}
 		}